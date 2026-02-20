ea41fb79fadf6c3e1e80787b9b6631dffe4acf0b
Vivekkumar <adityakumarsingh09@gmail.com>
2026-02-20 11:44:05 +0530

    feat: remove App component and related routing for application restructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,1 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { AppProvider } from './context/AppContext';
-import Dashboard from './pages/Dashboard';
-import ScreenDetail from './pages/ScreenDetail';
 
-function App() {
-  return (
-    <AppProvider>
-      <div className="min-h-screen bg-gray-50 text-gray-900 font-sans">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/details" element={<ScreenDetail />} />
-        </Routes>
-      </div>
-    </AppProvider>
-  );
-}
-export default App;
